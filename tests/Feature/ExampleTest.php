1aba72ca1b60778d8c716488faf7143387b77fd4
Rafi Chandra <crafi2606@gmail.com>
2025-03-23 05:43:19 +0700

    workflow ci/cd

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,7 +3,8 @@
 it('returns a successful response', function () {
     $response = $this->get('/');
     
-    $response->assertStatus(fn ($status) => 
-        in_array($status, [200, 302])
+    $this->assertTrue(
+        in_array($response->getStatusCode(), [200, 302]),
+        'Respons seharusnya 200 atau 302, tetapi mendapatkan ' . $response->getStatusCode()
     );
 });
